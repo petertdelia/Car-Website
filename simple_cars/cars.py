da1cd5710f51c2ff84112511ed979e145616d584
Peter D'Elia <petertdelia@gmail.com>
2019-07-15 14:26:10 -0400

    added larger db file

--- simple_cars/cars.py
+++ simple_cars/cars.py
@@ -11,7 +11,7 @@ def index():
 
     db = get_db()
     cars = db.execute(
-        'SELECT year,make,model,trim,mileage FROM cars'
+        'SELECT year,make,model,trim,drive,mileage,price FROM cars ORDER BY price'
     ).fetchall()
 
     return render_template('car_view/cars.html', cars=cars)
@@ -29,7 +29,7 @@ def search():
 
         if error is None:
             cars = db.execute(
-                'SELECT * FROM cars WHERE trim = ?', (trim,)
+                'SELECT * FROM cars WHERE trim = ? ORDER BY price', (trim,)
             )
             return render_template('car_view/cars.html', cars=cars)
         flash(error)
