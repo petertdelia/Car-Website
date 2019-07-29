a33ace0c6c8b73278326e1d2030efcd501275de5
Peter D'Elia <petertdelia@gmail.com>
2019-07-29 11:18:41 -0400

    page numbers now works for search view

--- simple_cars/cars.py
+++ simple_cars/cars.py
@@ -8,18 +8,24 @@ bp = Blueprint('cars', __name__)
 
 @bp.route('/cars', methods=('GET','POST'))
 def index():
+    page_number = int(request.args.get('page', '0'))
 
     db = get_db()
     cars = db.execute(
         'SELECT year,make,model,trim,drive,mileage,price FROM cars ORDER BY price'
     ).fetchall()
 
-    return render_template('car_view/cars.html', cars=cars)
+    if page_number == -1:
+        page_number = len(cars) / 30
+
+    cars = cars[30 * page_number : 30 * (page_number + 1)]
+
+    return render_template('car_view/cars.html', cars=cars, page_number=page_number)
 
 @bp.route('/cars/search', methods=('GET','POST'))
 def search():
     if request.method =='POST':
-        
+        page_number = int(request.args.get('page', '0'))
         trim = request.form['trim']
         db = get_db()
         error = None
@@ -30,8 +36,11 @@ def search():
         if error is None:
             cars = db.execute(
                 'SELECT * FROM cars WHERE trim = ? ORDER BY price', (trim,)
-            )
-            return render_template('car_view/cars.html', cars=cars)
+            ).fetchall()
+            if page_number == -1:
+                page_number = len(cars) / 30
+            cars = cars[30 * page_number : 30 * (page_number + 1)]
+            return render_template('car_view/cars.html', cars=cars, page_number=page_number)
         flash(error)
 
     db = get_db()
